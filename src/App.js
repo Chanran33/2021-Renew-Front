2d01f87d782d3dd4e2b33a5e87940f1652ebd237
Chanran33 <k55416325@gmail.com>
2021-03-02 17:08:40 +0900

    첫번째 합침

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import MapView from "./Pages/MapView/MapView";
-import { Route } from "react-router-dom";
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import About from './Pages/About';
+import './Styles/App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      <div class="header">
+        <img class="logo" src="image/home_logo2.png" alt="logo"/>
+        <nav>
+          <ul class="nav_links">
+            <li class="li"><Link class="link" to="/">Home</Link></li>
+            <li class="li"><Link class="link" to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <Link to="/signin"><button>Login →</button></Link>
+      </div>
+
       <div class="route">
-        <Route path="/mapview" component={MapView} />
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/about" component={About} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/CreateEvent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateEvent = () => {
+    return (
+        <div>
+            <h1>방가</h1>
+        </div>
+    );
+};
+
+export default CreateEvent;

4c3ebdb69eb38a82794b8e97bf9b0f8fd1824bc9
Chanran33 <k55416325@gmail.com>
2021-03-02 17:14:36 +0900

    수정된거 반영 후 올림

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
 import About from './Pages/About';
+import CreateEvent from './Pages/CreateEvent';
 import './Styles/App.css';
+
 
 const App = () => {
   return (
     <div>
       <div class="header">
-        <img class="logo" src="image/home_logo2.png" alt="logo"/>
+        <Link class="logo_link" to="/"><img class="logo" src="image/home_logo2.png" alt="logo"/></Link>
         <nav>
           <ul class="nav_links">
             <li class="li"><Link class="link" to="/">Home</Link></li>
@@ -24,7 +25,7 @@ const App = () => {
         <Route path="/" component={Home} exact={true} />
         <Route path="/about" component={About} />
         <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
+        <Route path="/createEvent" component={CreateEvent} />
       </div>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
 import '../Styles/Home.css';
 
 const Home = () => {
     return (
         <div class="Home_body">
             <div class="social">
-                <img src="image/facebook_logo.png" alt="facebook_logo" />
-                <img src="image/twitter_logo.png" alt="twitter_logo" />
+                <img src="image/facebook_logo.png" alt="facebook_logo" /><br />
+                <img src="image/twitter_logo.png" alt="twitter_logo" /><br />
                 <img src="image/instagram_logo.png" alt="instagram_logo" />
             </div>
 
             <div class="welcome">
-                <h1>Go green by Upcycling</h1>
+                <h1>Go green by<br/> Upcycling</h1>
                 <p>share Upcycling events with your neighbors and save the earth!</p>
-                <div>
-                    <button>Contents</button>
-                    <button>Create event</button>
-                    <img src="image/MapTrifold.png" alt="map"/>
+                <div class="welcome_buttons">
+                    <Link to="/"><button>Contents</button></Link>
+                    <Link to="/createevent"><button>Create event</button></Link>
+                    <Link to="/"><img src="image/MapTrifold.png" alt="map"/></Link>
                 </div>
             </div>
         </div>
